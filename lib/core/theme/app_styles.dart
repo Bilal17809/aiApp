c3e7100992976e02cb76c1df6d3033cd13235eb3
Bilal17809 <153565813+Bilal17809@users.noreply.github.com>
2025-07-14 10:15:19 +0500

    comment by Bilal_TL

--- lib/core/theme/app_styles.dart
+++ lib/core/theme/app_styles.dart
@@ -1,78 +1,120 @@
 import 'package:flutter/material.dart';
+import '../constant/constant.dart';
 import 'app_colors.dart';
 
-const TextStyle headlineLargeStyle = TextStyle(
-  fontSize: 124,
-  fontWeight: FontWeight.w700,
-  color: blackTextColor,
-);
 const TextStyle headlineMediumStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 30,
   fontWeight: FontWeight.w700,
   color: blackTextColor,
 );
 const TextStyle headlineSmallStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 24,
   fontWeight: FontWeight.w500,
   color: blackTextColor,
 );
 
-const TextStyle titleBoldMediumStyle = TextStyle(
-  fontSize: 20,
-  fontWeight: FontWeight.bold,
+const TextStyle titleLargeStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontSize: 22,
+  fontWeight: FontWeight.w500,
   color: blackTextColor,
 );
 
-const TextStyle titleBoldLargeStyle = TextStyle(
-  fontSize: 24,
-  fontWeight: FontWeight.bold,
-  color: blackTextColor,
-);
+const TextStyle titleMediumStyle = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: blackTextColor);
 
 const TextStyle titleSmallStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 16,
   fontWeight: FontWeight.w500,
-  color: blackTextColor,
-);
-
-const TextStyle titleSmallBoldStyle = TextStyle(
-  fontSize: 16,
-  fontWeight: FontWeight.bold,
   color: blackTextColor,
 );
 
 const TextStyle bodyLargeStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 16,
   fontWeight: FontWeight.w400,
 );
 
 const TextStyle bodyMediumStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 14,
   fontWeight: FontWeight.w400,
   color: blackTextColor,
 );
 
-const TextStyle bodyBoldMediumStyle = TextStyle(
+const TextStyle bodySmallStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle buttonTextStyle = TextStyle(
+  fontFamily: fontFamily,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle labelMediumStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 14,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w500,
   color: blackTextColor,
 );
-
-const TextStyle bodyBoldSmallStyle = TextStyle(
+const TextStyle labelSmallStyle = TextStyle(
+  fontFamily: fontFamily,
   fontSize: 12,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.w500,
+  color: blackTextColor,
 );
-
 //decoration
+
 final BoxDecoration roundedDecorationWithShadow = BoxDecoration(
-  color: kWhite,
+  color: Colors.white,
   borderRadius: BorderRadius.circular(10),
   boxShadow: [
     BoxShadow(
-      color: primaryColor.withValues(alpha: 0.3),
+      color: Colors.grey.withValues(alpha: 0.2),
       blurRadius: 6,
-      spreadRadius: 1,
       offset: Offset(0, 2),
     ),
   ],
+);
+final BoxDecoration roundedDecoration = BoxDecoration(
+  color: Colors.white,
+  borderRadius: BorderRadius.circular(10),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.grey.withValues(alpha: 0.2),
+      blurRadius: 6,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
+final BoxDecoration roundedGreenBorderDecoration = BoxDecoration(
+  color: greenColor.withValues(alpha: 0.3),
+  borderRadius: BorderRadius.circular(10),
+  border: Border.all(
+    color: greenColor,
+    width: 1.0,
+  ),
+);
+
+final BoxDecoration rounderGreyBorderDecoration = BoxDecoration(
+  color: kWhite,
+  borderRadius: BorderRadius.circular(12),
+  border: Border.all(
+    color: greyBorderColor,
+  ),
+);
+
+final boxShadow = BoxShadow(
+  color: Colors.grey.withValues(alpha: 0.2),
+  blurRadius: 6,
+  offset: Offset(0, 2),
 );
